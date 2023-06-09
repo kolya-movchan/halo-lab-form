c2534fb4c1e7aaf25fd590753675b8e1a9cb780f
Kolya Movchan <kmovchan261@gmail.com>
2023-06-09 13:59:51 +0300

    Clean Up + add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Header } from 'components/Header';
-import { Button } from 'components/Button';
-import { ReactComponent as Logo } from 'assets/favicon.svg';
+import { Form } from 'components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Header title="hola" />
-      <Logo height={100} width={100} />
-      <Button onClick={() => alert('hola')}>Heyo</Button>
+      <Form />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
 
 import './index.css';
 import App from './App';
@@ -8,8 +7,6 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   );
 }
