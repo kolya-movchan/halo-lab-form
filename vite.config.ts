356a99ed10fac1ea72916d2ab8229c4dea8ecfcc
Kolya Movchan <kmovchan261@gmail.com>
2023-06-11 21:36:06 +0300

    Fix Failed Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -28,15 +28,9 @@ import svgrPlugin from 'vite-plugin-svgr';
 export default defineConfig({
   envDir: './env',
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
-  /* If proxy is needed
-  server: {
-    proxy: {
-      "/api": "localhost:8080"
-    }
-  },
-  */
+  base: "/halo-lab-form",
+
   build: {
     sourcemap: true,
-    outDir: 'build', // Change the output folder name to 'build'
   },
 });
